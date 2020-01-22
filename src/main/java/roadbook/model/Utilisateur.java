a47d6f1c8910cb48e759721383a9f4014259830a
TwoNails <52172879+TwoNails@users.noreply.github.com>
2020-01-22 12:03:32 +0100

    Utilsateur

--- src/main/java/roadbook/model/Utilisateur.java
+++ src/main/java/roadbook/model/Utilisateur.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
