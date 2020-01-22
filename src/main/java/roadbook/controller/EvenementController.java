51bdcb701bedfefcddc00b504257c40e7d69b3ba
test <test@SIMPOSTE4-013>
2020-01-22 17:24:39 +0100

    Attention Commit de Sophie

--- src/main/java/roadbook/controller/EvenementController.java
+++ src/main/java/roadbook/controller/EvenementController.java
@@ -1,5 +1,53 @@
 package roadbook.controller;
 
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import roadbook.model.Evenement;
+import roadbook.repository.EvenementRepository;
+
+
+
+@RestController
 public class EvenementController {
+	
+	@Autowired
+	private EvenementRepository evenementRepository;
+	
+	@GetMapping("/evenements")
+	public Collection<Evenement> findAll(){
+		return evenementRepository.findAll();
+	}
+	
+	@GetMapping("/evenementById/{id}")
+	public Optional<Evenement> findById(@PathVariable Integer id) {
+		return evenementRepository.findById(id);
+	}
+	
+//	@GetMapping("/evenementByNom/{nom}")
+//	public Optional<Evenement> findByNom(@PathVariable String nom) {
+//		return evenementRepository.findByNom(nom);
+//	}
+//	
+	
+	/**
+	 * Ajoute un evenement
+	 */
+	@PostMapping("/addEvenement")
+	public Evenement ajoutEvenement(@RequestBody Evenement evenement){
+		return evenementRepository.saveAndFlush(evenement);
+	}
+	
+	//mettre a jour
+//	public Evenement updateEvenement(Evenement evenement){
+//		return evenementRepository.saveAndFlush(evenement);
+//	}
 
 }
--- src/main/java/roadbook/repository/EvenementRepository.java
+++ src/main/java/roadbook/repository/EvenementRepository.java
@@ -1,12 +1,17 @@
 package roadbook.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import roadbook.model.Evenement;
+import roadbook.model.Utilisateur;
 
 @Repository
 
 public interface EvenementRepository extends JpaRepository<Evenement, Integer> {
 
+//	public Optional<Evenement> findByNom(String nom);
+	
 }
