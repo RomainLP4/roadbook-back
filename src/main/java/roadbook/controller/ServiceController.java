4884e252223c56bb7598224235ca892385f552ae
Robear <robert.roussely.lp4@gmail.com>
2020-01-22 17:25:22 +0100

    service controller

--- src/main/java/roadbook/controller/ServiceController.java
+++ src/main/java/roadbook/controller/ServiceController.java
@@ -1,8 +1,13 @@
 package roadbook.controller;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +24,20 @@ public class ServiceController {
 
 		return serviceRepository.findAll();
 	}
+	@GetMapping("/serviceById/{id}")
+	public Optional<Service> findById(@PathVariable Integer id) {
+		return serviceRepository.findById(id);
+	}
+	
+	
+	@GetMapping("/servicesByCategorie/{categorie}")
+	public Optional<Service> findByCategorie(@PathVariable String categorie) {
+		return serviceRepository.findByCategorie(categorie);
+	}
+	
+	 
+	    @PostMapping("/addService")
+	    public Service ajoutEvenement(@RequestBody Service service){
+	        return serviceRepository.saveAndFlush(service);
+	    }
 }
--- src/main/java/roadbook/repository/ServiceRepository.java
+++ src/main/java/roadbook/repository/ServiceRepository.java
@@ -1,10 +1,12 @@
 package roadbook.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import roadbook.model.Service;
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, Integer> {
-
+	public Optional<Service> findByCategorie(String categorie);
 }
