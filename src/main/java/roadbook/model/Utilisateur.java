97c0e222b1b4eb0a9eb28c66a3557b2eb9fcee1f
Romain <romain.gallard.lp4@gmail.com>
2020-01-22 13:52:22 +0100

    user  model update

--- src/main/java/roadbook/model/Utilisateur.java
+++ src/main/java/roadbook/model/Utilisateur.java
@@ -11,10 +11,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Utilisateur {
 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,6 +28,126 @@ public class Utilisateur {
 	private String role; // ENUM ?
 	
 	
+	public Utilisateur(String pseudo, String email, String password, String nom, String prenom, String telephone,
+			String niveau, String ville, String role) {
+		super();
+		this.pseudo = pseudo;
+		this.email = email;
+		this.password = password;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.telephone = telephone;
+		this.niveau = niveau;
+		this.ville = ville;
+		this.role = role;
+	}
+
+
+	public Utilisateur() {
+		super();
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+
+	public String getNiveau() {
+		return niveau;
+	}
+
+
+	public void setNiveau(String niveau) {
+		this.niveau = niveau;
+	}
+
+
+	public String getVille() {
+		return ville;
+	}
+
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
 	@Override
 	public String toString() {
 		return "Utilisateur [id=" + id + ", pseudo=" + pseudo + ", email=" + email + ", password=" + password + ", nom="
