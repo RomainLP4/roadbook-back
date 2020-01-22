8a89e588e549497af532059e90a2b48a9349ae56
Romain <romain.gallard.lp4@gmail.com>
2020-01-22 15:36:36 +0100

    commit

--- src/main/java/roadbook/model/Utilisateur.java
+++ src/main/java/roadbook/model/Utilisateur.java
@@ -5,11 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
 public class Utilisateur {
 
