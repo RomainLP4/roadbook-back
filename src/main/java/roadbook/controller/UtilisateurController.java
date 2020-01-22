721d751b2831361edfa3d87b4de34186d9e7cd74
TwoNails <52172879+TwoNails@users.noreply.github.com>
2020-01-22 12:23:32 +0100

    UserController

--- src/main/java/roadbook/controller/UtilisateurController.java
+++ src/main/java/roadbook/controller/UtilisateurController.java
@@ -1,6 +1,7 @@
 package roadbook.controller;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,4 +20,10 @@ public class UtilisateurController {
 	public Collection<Utilisateur> findAll(){
 		return utilisateurRepository.findAll();
 	}
+	
+	@GetMapping("/utilisateur/{id}")
+	public Optional<Utilisateur> findById(Integer id) {
+		return utilisateurRepository.findById(id);
+	}
+	
 }
