db19c822da759d2befd7c2333a037274fe73a308
Romain <romain.gallard.lp4@gmail.com>
2020-01-22 10:34:18 +0100

    app.properties

--- src/main/java/roadbook/controller/RegionController.java
+++ src/main/java/roadbook/controller/RegionController.java
@@ -1,5 +1,13 @@
 package roadbook.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import roadbook.repository.RegionRepository;
+
+@RestController
 public class RegionController {
 
+	@Autowired
+	private RegionRepository regionRepository;
 }
