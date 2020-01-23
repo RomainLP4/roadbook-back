21d2176580f100d0c86e176ed12cc727fb18c0e8
Romain <romain.gallard.lp4@gmail.com>
2020-01-23 10:49:12 +0100

    del 1 moto

--- src/main/java/roadbook/controller/ArticleController.java
+++ src/main/java/roadbook/controller/ArticleController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import roadbook.model.Article;
@@ -27,4 +28,13 @@ public class ArticleController {
 	public Optional<Article> findByTag(@PathVariable String tag) {
 		return articleRepository.findByTag(tag);
 	}
+
+
+	  
+
+
+
+
+
 }
+
--- src/main/java/roadbook/controller/MotoController.java
+++ src/main/java/roadbook/controller/MotoController.java
@@ -1,7 +1,11 @@
 package roadbook.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roadbook.model.Moto;
 import roadbook.repository.MotoRepository;
@@ -17,5 +21,17 @@ public class MotoController {
 		return motoRepository.findAll();
 	}
 
+	
+	
+	@RequestMapping("/delMoto/{id}")
+    public void delOne(@PathVariable int id) {
+        Optional<Moto> optMoto = motoRepository.findById(id);
+        if (optMoto.isPresent()) {
+            motoRepository.deleteById(id);
+            System.out.println("Moto supprimée");
+        } else {
+            System.out.println("Pas de moto avec cet ID");
+        }
+    }
 }
 
