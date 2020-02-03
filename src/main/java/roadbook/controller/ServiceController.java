97672127dafc84595aef01e3eac46380bb838bac
Robear <robert.roussely.lp4@gmail.com>
2020-02-03 10:56:26 +0100

    ajout crossorigin

--- src/main/java/roadbook/controller/ServiceController.java
+++ src/main/java/roadbook/controller/ServiceController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ import roadbook.model.Evenement;
 import roadbook.model.Service;
 import roadbook.model.Utilisateur;
 import roadbook.repository.ServiceRepository;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ServiceController {
 	@Autowired
