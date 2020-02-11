7fcfb23e3964e87584c6bf7ba12855123a1c0748
Sophie85 <sophie.balcon.lp4@gmail.com>
2020-02-11 15:07:15 +0100

    commit region

--- src/main/java/roadbook/controller/RegionController.java
+++ src/main/java/roadbook/controller/RegionController.java
@@ -3,6 +3,7 @@ package roadbook.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import roadbook.model.Region;
 import roadbook.repository.RegionRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class RegionController {
 
 	@Autowired
