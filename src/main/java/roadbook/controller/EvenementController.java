f963f38dfffbfa9be9f87be45701085ac3208552
Sophie85 <sophie.balcon.lp4@gmail.com>
2020-02-13 11:03:38 +0100

    Commit de Sophie

--- src/main/java/roadbook/controller/EvenementController.java
+++ src/main/java/roadbook/controller/EvenementController.java
@@ -5,18 +5,17 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import roadbook.model.Evenement;
-import roadbook.model.Service;
 import roadbook.repository.EvenementRepository;
 
 
@@ -105,9 +104,9 @@ public class EvenementController {
 //		return evenementRepository.saveAndFlush(evenement);
 //	}
 	
-	@PostMapping("/updateEvenement")
+	@PutMapping("/updateEvenement")
 	public ResponseEntity<Evenement> updateEvenement(@RequestBody Evenement evenement) {
-		
+
 		Optional<Evenement> evenementEnBase = evenementRepository.findById(evenement.getId());
 			
 		if(evenementEnBase.isPresent()) {	
