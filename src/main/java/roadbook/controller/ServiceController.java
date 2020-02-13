6d032ac6cb606a65bcc4d57355b92bcf2086e597
Robear <robert.roussely.lp4@gmail.com>
2020-02-13 14:50:29 +0100

    Update services

--- src/main/java/roadbook/controller/ServiceController.java
+++ src/main/java/roadbook/controller/ServiceController.java
@@ -6,13 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import roadbook.model.Evenement;
 import roadbook.model.Service;
@@ -75,7 +69,7 @@ public class ServiceController {
 //		            return updateService;
 //		        }
 //		    }
-		@PostMapping("/updateService")
+		@PutMapping("/updateService")
 		public ResponseEntity<Service> updateService(@RequestBody Service service) {
 		// public Utilisateur utilisateurUpdateProfile(@RequestBody Utilisateur utilisateur) {
 			
